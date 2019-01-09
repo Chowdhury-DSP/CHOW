0dbfbb73f858241955025cf6f3ddae623900e5b8
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2019-01-08 23:29:09 -0800

    Implement original Chow algorithm

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -129,32 +129,34 @@ bool ChowAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) con
 }
 #endif
 
+float ChowAudioProcessor::chow (float x)
+{
+    float cntrl = 1.0f;
+    float makeup = 1.6f;
+    float thresh = 0.05f;
+    float ratio = 10.0f;
+
+    //Set control voltage
+    if (x > thresh){
+        cntrl = thresh + ((x - thresh) / ratio);		
+    }
+    else { cntrl = x; }
+
+    //set y
+    return cntrl * makeup;
+}
+
 void ChowAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
 {
     ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
+        auto* x = buffer.getWritePointer (channel);
+        for (int n = 0; n < buffer.getNumSamples(); n++)
+            x[n] = chow (x[n]);
     }
 }
 
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -56,6 +56,9 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    float chow (float x);
+
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ChowAudioProcessor)
 };
